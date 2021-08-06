156a8f10136680c4f8d38320d3269869b1ec5411
Kesara Rathnayake <kesara@fq.nz>
2021-08-06 14:42:53 +1200

    Implements file submit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+document.getElementById('buttonSubmit').onclick = function() {
+  const formData = new FormData();
+  formData.append('file', document.getElementById('formFile').files[0]);
+
+  const apiCall = 'http://devbox.amsl.com:55555/api/render/' + document.getElementById('selectFormat').value;
+
+  const request = new Request(apiCall, {
+    method: 'POST',
+    body: formData
+  });
+
+  fetch(request)
+    .then(response => response.blob())
+    .then(blob => URL.createObjectURL(blob))
+    .then(url => {
+        window.open(url);
+        URL.revokeObjectURL(url);
+    });
+}
