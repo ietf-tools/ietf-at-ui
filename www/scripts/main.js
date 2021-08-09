168b0ab99c6a111aeaba79273514d813d6596868
Kesara Rathnayake <kesara@fq.nz>
2021-08-09 13:06:10 +1200

    Adds user friendly dowload and error handling

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,8 +1,40 @@
+const alertError  = document.getElementById('alertError');
+const buttonDownload = document.getElementById('buttonDownload');
+const formFile = document.getElementById('formFile');
+const messageError = document.getElementById('messageError');
+const selectFormat = document.getElementById('selectFormat');
+
+reset();
+
+formFile.addEventListener('change', reset);
+selectFormat.addEventListener('change', reset);
+
+function getDownloadFilename(file, format) {
+  if (format == 'text') {
+    format = 'txt';
+  }
+
+  return file.name + '.' + format;
+}
+
+function reset() {
+  alertError.style.display = 'none';
+  buttonDownload.style.display = 'none';
+  buttonDownload.setAttribute('download', '');
+  buttonDownload.href = '#';
+  messageError.innerHTML = '';
+}
+
 document.getElementById('buttonSubmit').onclick = function() {
   const formData = new FormData();
-  formData.append('file', document.getElementById('formFile').files[0]);
+  const format = selectFormat.value;
+  const file = formFile.files[0];
 
-  const apiCall = 'http://devbox.amsl.com:55555/api/render/' + document.getElementById('selectFormat').value;
+  formData.append('file', file);
+
+  reset();
+
+  const apiCall = 'http://devbox.amsl.com:55555/api/render/' + format;
 
   const request = new Request(apiCall, {
     method: 'POST',
@@ -11,9 +43,24 @@ document.getElementById('buttonSubmit').onclick = function() {
 
   fetch(request)
     .then(response => response.blob())
-    .then(blob => URL.createObjectURL(blob))
-    .then(url => {
-        window.open(url);
-        URL.revokeObjectURL(url);
+    .then(blob => {
+      if (blob.type == 'application/json') {
+        alertError.style.display = 'block';
+        return blob.text();
+      }
+      else {
+        return URL.createObjectURL(blob);
+      }
+    })
+    .then(data => {
+      try {
+        data = JSON.parse(data);
+        messageError.innerHTML = data.error;
+      } catch (error) {
+        // file rendering is successful
+        buttonDownload.style.display = 'block';
+        buttonDownload.setAttribute('download', getDownloadFilename(file, format));
+        buttonDownload.href = data;
+      }
     });
 }
