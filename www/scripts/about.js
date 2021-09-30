8a81a049a0407632f30e5175bd9d0cbc6e82fe14
Kesara Rathnayake <kesara@fq.nz>
2021-10-01 11:35:43 +1300

    Adds about.html
    New about.html page gets version information from IETF @ API and displays tool
    information.

---
+++ www/scripts/about.js
@@ -0,0 +1,27 @@
+const alertError  = document.getElementById('alertError');
+const messageError = document.getElementById('messageError');
+
+alertError.style.display = 'none';
+messageError.innerHTML = '';
+
+const apiCall = 'https://author-tools.ietf.org/api2/version/';
+
+const request = new Request(apiCall, {
+  method: 'GET',
+});
+
+fetch(request)
+  .then(function(response) { return response.json(); })
+  .then(function(json) {
+    document.getElementById('spanIetfat').innerHTML = json.versions.author_tools_api;
+    document.getElementById('spanXml2rfc').innerHTML = json.versions.xml2rfc;
+    document.getElementById('spanKramdown').innerHTML = json.versions['kramdown-rfc2629'];
+    document.getElementById('spanMmark').innerHTML = json.versions.mmark;
+    document.getElementById('spanId2xml').innerHTML = json.versions.id2xml;
+    document.getElementById('spanWeasyprint').innerHTML = json.versions.weasyprint;
+    document.getElementById('spanGoat').innerHTML = json.versions.goat;
+  })
+  .catch(error => {
+      alertError.style.display = 'block';
+      messageError.innerHTML = 'Error occured while retrieving version infomation.';
+  });
